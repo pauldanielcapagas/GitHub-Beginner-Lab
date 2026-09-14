4ee75a08dd542dbbc35250306203c0fdf582279d
hal. pauldanielcapagas <pauldanielcapagas@gmail.com>
2026-09-14 21:53:39 +0800

    okay napo yung form

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(150, 37);
+            this.label1.Location = new System.Drawing.Point(106, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(499, 32);
             this.label1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(187, 95);
+            this.label2.Location = new System.Drawing.Point(158, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(399, 32);
             this.label2.TabIndex = 1;
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(707, 196);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
