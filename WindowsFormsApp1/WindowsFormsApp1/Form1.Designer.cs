5ae07b1d115eb5387f61fc4b23e886aa7e9430c8
hal. pauldanielcapagas <pauldanielcapagas@gmail.com>
2026-09-14 21:05:31 +0800

    Update student profile output

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(143, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(489, 32);
+            this.label1.Size = new System.Drawing.Size(499, 32);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Profile —Paul Daniel Capagas";
+            this.label1.Text = "Student Profile — GitHub Beginner Lab";
             // 
             // Form1
             // 
